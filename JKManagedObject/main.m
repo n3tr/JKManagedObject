2e2ffc68e14edd643ac99d0e1373ece2807e35fc
Jirat K <saakyz@gmail.com>
2012-08-29 12:44:53 +0700

    Initial Commit

---
+++ JKManagedObject/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JKManagedObject
+//
+//  Created by Jirat K on 8/29/55 BE.
+//  Copyright (c) 2555 Allianz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
